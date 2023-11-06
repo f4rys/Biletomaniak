ed97f018461db55f8ae29751cc0c7c622187b167
f4rys <looxxon@gmail.com>
2023-11-06 16:48:08 +0100

    better UI

--- src/app/booking-success/booking-success.component.ts
+++ src/app/booking-success/booking-success.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FirebaseService } from '../firebase.service';
 import { Router } from "@angular/router";
+import {Clipboard} from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-booking-success',
@@ -10,11 +11,18 @@ import { Router } from "@angular/router";
 })
 export class BookingSuccessComponent {
   booking_id: any;
-  constructor(private firebase_service: FirebaseService, private activatedRoute: ActivatedRoute, private router: Router) {}
+  copyMessage = "Kopiuj"
+  constructor(private firebase_service: FirebaseService, private activatedRoute: ActivatedRoute, private router: Router, private clipboard: Clipboard) {}
 
   ngOnInit() {
       this.activatedRoute.queryParams.subscribe((booking_id) => {
       this.booking_id = booking_id
       });
   }
+
+  copyBookingID() {
+    this.clipboard.copy(this.booking_id.booking_id);
+    this.copyMessage = "Skopiowano"
+  }
+
 }
