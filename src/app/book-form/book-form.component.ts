33616ac7d6a74c5ad518abe88d214b2632eb5f2f
f4rys <looxxon@gmail.com>
2024-06-18 19:58:59 +0200

    Better code readability

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -9,9 +9,7 @@ import { Router } from "@angular/router";
 })
 export class BookFormComponent {
   constructor(private firebase_service: FirebaseService, private router: Router) {}
-
   events: any
-
   first_name!: string;
   last_name!: string;
   phone_number!: string;
@@ -20,12 +18,10 @@ export class BookFormComponent {
   
   async ngOnInit() {
     this.events = await this.firebase_service.getEvents()
-    console.log(this.events)
     };
 
   async onSubmit() {
       const booking_id = await this.firebase_service.addBooking(this.first_name, this.last_name, this.email, this.phone_number, this.event_id)
       this.router.navigate(['/booking-success'], { queryParams: {booking_id} });
     }
-
 }
