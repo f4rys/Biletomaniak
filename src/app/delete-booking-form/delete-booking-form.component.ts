9b1cc999a135ae8154dea6487f930ec7d9ed6806
f4rys <looxxon@gmail.com>
2024-07-22 11:23:28 +0200

    Handle reservation not found

--- src/app/delete-booking-form/delete-booking-form.component.ts
+++ src/app/delete-booking-form/delete-booking-form.component.ts
@@ -11,10 +11,15 @@ export class DeleteBookingFormComponent {
   constructor(private firebase_service: FirebaseService, private router: Router) {}
 
   booking_id!: string;
+  errorMessage: string | null = null;
 
   async onSubmit() {
-    let data = await this.firebase_service.getBooking(this.booking_id)
-    this.router.navigate(['/show-booking'], { queryParams: data });
-    };
-    
+    try {
+      const data = await this.firebase_service.getBooking(this.booking_id);
+      this.router.navigate(['/show-booking'], { queryParams: data });
+      this.errorMessage = null;
+    } catch (error) {
+      this.errorMessage = 'Nie odnaleziono rezerwacji. Sprawdź poprawność identyfikatora lub skontaktuj się z administracją.';
+    }
   }
+}
